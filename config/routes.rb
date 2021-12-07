6fd8aae82362979c8c899e5a0cccf23a083dd54d
fe3524 <fe212045@grace.ocn.ne.jp>
2021-12-07 23:15:06 +0900

    devise_controller/view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
+  devise_for :administrators, controllers: {
+    sessions: 'users/sessions'
+  }
 
-  devise_for :users
-  devise_for :administrators
 end
