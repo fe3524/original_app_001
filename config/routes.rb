65c754caaee384f365a8cbd5a56ffbda9b83bc77
fe3524 <fe212045@grace.ocn.ne.jp>
2021-12-08 09:30:10 +0900

    トップページ／テスト用ビュー実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   root 'top#index'
+  resources :administrator_posts
+  resources :user_posts
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
   }
 
   devise_for :administrators, controllers: {
-    sessions: 'administrators/sessions'
+    sessions: 'administrators/sessions',
+    passwords: 'administrators/passwords',
+    registrations: 'administrators/registrations'
   }
 
 end
